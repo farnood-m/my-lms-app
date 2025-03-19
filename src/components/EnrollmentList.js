0c335e755f213a20f41fd50033e49b04020a5d10
farnood-m <mojahedzadeh.f@gmail.com>
2025-03-19 15:50:04 -0600

    minor fix

--- src/components/EnrollmentList.js
+++ src/components/EnrollmentList.js
@@ -2,18 +2,15 @@ import React from "react";
 import EnrolledCourse from "./EnrolledCourse";
 
 const EnrollmentList = ({ enrolledCourses, onDropCourse }) => {
- 
-
   const totalCreditHours = enrolledCourses.reduce((total, course) => {
     const creditHours = parseInt(course.duration.charAt(0), 10) || 0;
     return total + creditHours;
   }, 0);
 
-
   return (
     <div className="enrollment-list">
-      <h3>Total Credit Hours: {totalCreditHours}</h3>
       <h2>Enrolled Courses</h2>
+      <h3>Total Credit Hours: {totalCreditHours}</h3>
       <table className="cv">
         <tbody>
           {enrolledCourses.map((course) => (
